922844c77f02aeb8ad1b116e23dc025cbe41c6af
cantsaydorifto <108617883+cantsaydorifto@users.noreply.github.com>
2024-03-18 12:52:31 +0530

    add Error Component

---
+++ src/components/cards/Error.tsx
@@ -0,0 +1,14 @@
+export default function Error() {
+    return (
+        <div
+            style={{ width: "calc(100% - 100px)" }}
+            className="m-auto max-w-[1000px] bg-white flex h-[600px] flex-col items-center justify-center gap-6 rounded-lg p-5 shadow-2xl"
+        >
+            <h1 className="text-6xl font-semibold text-orange-600">404</h1>
+            <p className="text-2xl font-medium">Page not found</p>
+            <button className="text-shadow flex h-11 items-center justify-center rounded-lg bg-gradient-to-b from-[#FFC896] from-[-47.73%] to-[#FB652B]  to-[78.41%] px-8 text-sm text-white">
+                Back to Home
+            </button>
+        </div>
+    );
+}
