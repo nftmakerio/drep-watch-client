ab714ad8fc95169d33c0150310b02a0efcad2846
MoFayaz <fayazm0ist@gmail.com>
2024-03-26 15:03:02 +0530

    endpoint update

--- src/data/api.ts
+++ src/data/api.ts
@@ -1,2 +1,2 @@
-export const BASE_API_URL = process.env.NODE_ENV === "production" ? "https://test-drep-2f2a280ac64a.herokuapp.com" : "http://localhost:8080"
-// export const BASE_API_URL = "https://test-drep-2f2a280ac64a.herokuapp.com"
+export const BASE_API_URL = process.env.NODE_ENV === "production" ? "https://sea-turtle-app-du5i9.ondigitalocean.app" : "http://localhost:8080"
+// export const BASE_API_URL = "https://sea-turtle-app-du5i9.ondigitalocean.app"
